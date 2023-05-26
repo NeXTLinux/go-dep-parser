221b3fd3493069de93ccabc435ee8b9c28d67947
w3security <99838653+cyberoslab@users.noreply.github.com>
2023-05-26 15:04:42 -0700

    update

---
+++ pkg/nodejs/npm/parse_test.go
@@ -0,0 +1,86 @@
+package npm
+
+import (
+	"os"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/nextlinux/go-dep-parser/pkg/types"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name     string
+		file     string // Test input file
+		want     []types.Library
+		wantDeps []types.Dependency
+	}{
+		{
+			name:     "lock version v1",
+			file:     "testdata/package-lock_v1.json",
+			want:     npmV1Libs,
+			wantDeps: npmDeps,
+		},
+		{
+			name:     "lock version v2",
+			file:     "testdata/package-lock_v2.json",
+			want:     npmV2Libs,
+			wantDeps: npmDeps,
+		},
+		{
+			name:     "lock version v3",
+			file:     "testdata/package-lock_v3.json",
+			want:     npmV2Libs,
+			wantDeps: npmDeps,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, err := os.Open(tt.file)
+			require.NoError(t, err)
+
+			got, deps, err := NewParser().Parse(f)
+			require.NoError(t, err)
+
+			sortLibs(got)
+			sortLibs(tt.want)
+
+			assert.Equal(t, tt.want, got)
+			if tt.wantDeps != nil {
+				sortDeps(deps)
+				sortDeps(tt.wantDeps)
+				assert.Equal(t, tt.wantDeps, deps)
+			}
+		})
+	}
+}
+
+func sortDeps(deps []types.Dependency) {
+	sort.Slice(deps, func(i, j int) bool {
+		return strings.Compare(deps[i].ID, deps[j].ID) < 0
+	})
+
+	for i := range deps {
+		sort.Strings(deps[i].DependsOn)
+	}
+}
+
+func sortLibs(libs []types.Library) {
+	for _, lib := range libs {
+		sortLocations(lib.Locations)
+	}
+	sort.Slice(libs, func(i, j int) bool {
+		return strings.Compare(libs[i].ID, libs[j].ID) < 0
+	})
+}
+
+func sortLocations(locs []types.Location) {
+	sort.Slice(locs, func(i, j int) bool {
+		return locs[i].StartLine < locs[j].StartLine
+	})
+}
---
+++ pkg/nodejs/packagejson/parse.go
@@ -0,0 +1,68 @@
+package packagejson
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/nextlinux/go-dep-parser/pkg/utils"
+
+	"github.com/nextlinux/go-dep-parser/pkg/types"
+	"golang.org/x/xerrors"
+)
+
+type packageJSON struct {
+	Name                 string            `json:"name"`
+	Version              string            `json:"version"`
+	License              interface{}       `json:"license"`
+	Dependencies         map[string]string `json:"dependencies"`
+	OptionalDependencies map[string]string `json:"optionalDependencies"`
+}
+
+type Package struct {
+	types.Library
+	Dependencies         map[string]string
+	OptionalDependencies map[string]string
+}
+
+type Parser struct{}
+
+func NewParser() *Parser {
+	return &Parser{}
+}
+
+func (p *Parser) Parse(r io.Reader) (Package, error) {
+	var pkgJSON packageJSON
+	if err := json.NewDecoder(r).Decode(&pkgJSON); err != nil {
+		return Package{}, xerrors.Errorf("JSON decode error: %w", err)
+	}
+
+	// Name and version fields are optional
+	// https://docs.npmjs.com/cli/v9/configuring-npm/package-json#name
+	if pkgJSON.Name == "" || pkgJSON.Version == "" {
+		return Package{}, nil
+	}
+
+	return Package{
+		Library: types.Library{
+			ID:      utils.PackageID(pkgJSON.Name, pkgJSON.Version),
+			Name:    pkgJSON.Name,
+			Version: pkgJSON.Version,
+			License: parseLicense(pkgJSON.License),
+		},
+		Dependencies:         pkgJSON.Dependencies,
+		OptionalDependencies: pkgJSON.OptionalDependencies,
+	}, nil
+}
+
+func parseLicense(val interface{}) string {
+	// the license isn't always a string, check for legacy struct if not string
+	switch v := val.(type) {
+	case string:
+		return v
+	case map[string]interface{}:
+		if license, ok := v["type"]; ok {
+			return license.(string)
+		}
+	}
+	return ""
+}
